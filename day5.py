26f844fd6fc814174397bf40a19ce290738828b9
vani-ghb <somavarapuvani1@gmail.com>
2025-02-04 20:43:30 -0800

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,92 @@
+#naming_convention
+#pascal case, snake case, camel case
+
+#pascale case--->
+#humanbeing-HumanBeing
+#addfunction-AddFunction
+#keyword-KeyWord
+#in classes we are used in pascal cases
+
+#snake case---->
+#varibles and function name -we are used in snake case
+#humanbeing-human being
+#add_function_result
+#it indicates the underscore_
+
+#camel case----->
+#humanBeing,addFunction
+#same like as pascal case but first letter is small and second class is capital like a camel 
+#in python we are not used in camel cases and in java we used
+#try to follow pascal case or snake case
+#try to give some context while defining varibles
+
+#constants---->
+#PIE=3.14--->it is a constant
+#PIE=2.14-->we can change the pie value and computer allows to change the value
+# db_password = "123456"
+# db_username = "vani"#snake case
+# DB_PASSWORD ="123456"
+# DB_USERNAME ="VANI"# pascal case
+
+#conversions , Nubersystems
+#string int,range to list,bool to int
+
+#int to float--->
+int1=5
+int1=float(int1)
+print(int1)
+int1=int(int1)
+print(int1)
+
+#int to bool--->
+bool1=True
+print(int(bool1))
+bool1=True
+print(bool(1))#true
+print(bool(0))#false
+print(bool(2))#true
+print(bool(-1))#true
+print(bool(-22))#true
+
+#truthy values and falsy values
+#truthy values -all numbers are truthy,true,strings 
+#falsy values- 0,falsy '' 
+
+#boolt to string---->
+print(bool("     "))#true
+print(bool('hello'))#true
+print(bool(''))#false
+
+print("-----string to int-----")
+
+#string to int--->
+print(int('25'))
+# print(int('25a'))#invalid literal for int() with base 10:'25a'
+# print(int('25.555'))#in int we can not convert the string into int
+
+#string to float---->
+print(float('25.555'))#error-i
+# int1=float(input())
+# print(int1)
+
+#string to list---->
+str1='vani'#including spaces are allowed 
+print(len(str1))
+print(list(str1))
+
+#list to string
+list1=[1,2, True,[1,6.72]]
+str1=str(list1)
+print(str(list1))
+print(len(list1))
+print(str1)
+print(str1[0])
+#assignment----------------------------------------------------->
+#int float bool complex list
+#int true true
+#float
+#bool
+#complex
+#list
+
+
